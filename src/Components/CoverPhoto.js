3171daa1751d306304e7b7bb45b54a7ba2b14e8e
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2022-01-15 13:16:33 +0600

    Update CoverPhoto.js

--- src/Components/CoverPhoto.js
+++ src/Components/CoverPhoto.js
@@ -3,15 +3,15 @@ import React, { Component } from 'react'
 export default class CoverPhoto extends Component {
     render() {
         return (
-            <div id="hero" class="hero route bg-image" style={{backgroundImage: "url(assets/img/hero-bg2.jpg)"}}>
-                <div class="overlay-itro"></div>
-                <div class="hero-content display-table">
-                    <div class="table-cell">
-                        <div class="container">
-                            <p class="display-6 color-d">Hello, world!</p>
-                            <h1 class="hero-title mb-4">I am Rahima Akter</h1>
-                            <p class="hero-subtitle"><span class="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span></p>
-                            {/* <p class="pt-3"><a class="btn btn-primary btn js-scroll px-4" href="#about" role="button">Learn More</a></p> */}
+            <div id="hero" className="hero route bg-image" style={{backgroundImage: "url(assets/img/hero-bg2.jpg)"}}>
+                <div className="overlay-itro"></div>
+                <div className="hero-content display-table">
+                    <div className="table-cell">
+                        <div className="container">
+                            <p className="display-6 color-d">Hello, world!</p>
+                            <h1 className="hero-title mb-4">I am Rahima Akter</h1>
+                            <p className="hero-subtitle"><span className="typed" data-typed-items="Designer, Developer, Freelancer, Photographer"></span></p>
+                            {/* <p className="pt-3"><a className="btn btn-primary btn js-scroll px-4" href="#about" role="button">Learn More</a></p> */}
                         </div>
                     </div>
                 </div>
