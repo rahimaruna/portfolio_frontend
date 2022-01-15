6a7c217a380d39a386647bf9fafdd2f1aec172a9
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2022-01-15 12:37:28 +0600

    navigation component

---
+++ src/Components/Index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Navbar from './Navbar'
+
+export default class Index extends Component {
+    render() {
+        return (
+            <>
+                <Navbar />
+            </>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Index from './Components/Index';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Index />
   </React.StrictMode>,
   document.getElementById('root')
 );
