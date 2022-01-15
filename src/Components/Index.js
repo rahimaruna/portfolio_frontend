9586f251c6bbb0a2efbdfaaed87df0d1fc49e608
Mahidul Moon <49182474+mahidulmoon@users.noreply.github.com>
2022-01-15 16:27:54 +0600

    my services section added

--- src/Components/Index.js
+++ src/Components/Index.js
@@ -3,6 +3,7 @@ import AboutMe from './AboutMe'
 import ContactMe from './ContactMe'
 import CoverPhoto from './CoverPhoto'
 import Footer from './Footer'
+import MyServices from './MyServices'
 import Navbar from './Navbar'
 
 export default class Index extends Component {
@@ -12,6 +13,7 @@ export default class Index extends Component {
                 <Navbar />
                 <CoverPhoto />
                 <AboutMe />
+                <MyServices />
                 <ContactMe />
                 <Footer />
             </>
---
+++ src/Components/MyServices.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react'
+
+export default class MyServices extends Component {
+    render() {
+        return (
+            <div>
+                <section id="services" class="services-mf pt-5 route">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="title-box text-center">
+                                    <h3 class="title-a">
+                                        My Services
+                                    </h3>
+                                    <p class="subtitle-a">
+                                        The Products that I am able to submit yet.
+                                    </p>
+                                    <div class="line-mf"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            {/* <div class="col-md-4">
+                                <div class="service-box">
+                                    <div class="service-ico">
+                                        <span class="ico-circle"><i class="bi bi-briefcase"></i></span>
+                                    </div>
+                                    <div class="service-content">
+                                        <h2 class="s-title">Web Design</h2>
+                                        <p class="s-description text-center">
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia,
+                                            provident vitae! Magni
+                                            tempora perferendis eum non provident.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div> */}
+                            {/* <div class="col-md-4">
+                                <div class="service-box">
+                                    <div class="service-ico">
+                                        <span class="ico-circle"><i class="bi bi-card-checklist"></i></span>
+                                    </div>
+                                    <div class="service-content">
+                                        <h2 class="s-title">Web Development</h2>
+                                        <p class="s-description text-center">
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia,
+                                            provident vitae! Magni
+                                            tempora perferendis eum non provident.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div> */}
+                            {/* <div class="col-md-4">
+                                <div class="service-box">
+                                    <div class="service-ico">
+                                        <span class="ico-circle"><i class="bi bi-bar-chart"></i></span>
+                                    </div>
+                                    <div class="service-content">
+                                        <h2 class="s-title">Photography</h2>
+                                        <p class="s-description text-center">
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia,
+                                            provident vitae! Magni
+                                            tempora perferendis eum non provident.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div> */}
+                            <div class="col-md-4">
+                                <div class="service-box">
+                                    <div class="service-ico">
+                                        <span class="ico-circle"><i class="bi bi-binoculars"></i></span>
+                                    </div>
+                                    <div class="service-content">
+                                        <h2 class="s-title">SQA</h2>
+                                        <p class="s-description text-center">
+                                        “Software never was perfect and won’t get perfect. But is that a license to create garbage? The missing ingredient is our reluctance to quantify quality.”<br/>— Boris Beizer
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="service-box">
+                                    <div class="service-ico">
+                                        <span class="ico-circle"><i class="bi bi-brightness-high"></i></span>
+                                    </div>
+                                    <div class="service-content">
+                                        <h2 class="s-title">ML Reasearch Associate</h2>
+                                        <p class="s-description text-center">
+                                        “Machine intelligence is the last invention that humanity will ever need to make.”<br/> - Nick Bostrom
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="service-box">
+                                    <div class="service-ico">
+                                        <span class="ico-circle"><i class="bi bi-calendar4-week"></i></span>
+                                    </div>
+                                    <div class="service-content">
+                                        <h2 class="s-title">Marketing Executive.</h2>
+                                        <p class="s-description text-center">
+                                        “No great marketing decisions have ever been made on qualitative data.”<br/> – John Sculley
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        )
+    }
+}
